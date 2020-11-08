ead6fe4cbc2b77c0f43b4a393434d66441718562
jianzhihuang <p0921442701@gmail.com>
2020-11-08 15:26:36 +0800

    1st version

--- AssertionExample.java
+++ AssertionExample.java
@@ -12,10 +12,7 @@ public class AssertionExample {
     	ans = 1.0/d + 100;
 	System.out.println("AA");
 
-	System.out.println("BB");
-
-	System.out.println("BB");
-	System.out.println("CC");
+	
 
     	//assert ans != Double.POSITIVE_INFINITY : "­pºâ¿ù»~! " + ans;
     	return ans;
